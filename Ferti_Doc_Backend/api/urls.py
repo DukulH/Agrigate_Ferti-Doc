3ee512d681d832e43dfb50cabbca9e6ff18a6479
dukulAgrigate <dukul.agrigate@gmail.com>
2023-10-20 17:25:10 +0600

    fixing backend

--- Ferti_Doc_Backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('predict/', views.PredictView.as_view()),
-    path('shop_details/<str:district>/', views.ShopDetailsByDistrict.as_view()),
-    path('orders/', views.CreateOrderView.as_view()),
-]
